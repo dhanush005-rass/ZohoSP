6d39745a4c68aeed901b4123c0236417bb241be0
Dhanush Kumar <gdhanushkumar2005@gmail.com>
2026-02-06 12:32:02 +0530

    Delete Arduino directory

--- Arduino/libraries/TcMenuLog/src/TcMenuLog.h
+++
@@ -1,7 +0,0 @@
-#ifndef TC_MENU_LOG_H
-#define TC_MENU_LOG_H
-
-// simply include the IoLogging file, this allows the header to match the library name
-#include "IoLogging.h"
-
-#endif //TC_MENU_LOG_H
